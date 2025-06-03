200909dcdfd45712f17b87d437894c4d546be15f
Rafaéla Jàneczko <sagethirteen@gmail.com>
2025-06-03 19:55:16 -0300

    feat: add and configure swagger

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,24 @@ import {
   NestFastifyApplication,
   FastifyAdapter,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Desafio API Logística')
+    .setDescription('Desafio técnico')
+    .setVersion('1.0')
+    .addTag('logística  ')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
